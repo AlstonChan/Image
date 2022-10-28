c9002354485139f7577eff5be78e344c8a18190d
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2022-10-28 22:59:34 +0800

    added warnings for invalid value

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,35 @@ export interface Props {
   w?: string;
   className?: string;
   objFit?: string;
-  objOps?: string;
+  objPos?: string;
   priority?: boolean;
 }
 
 export default function Image(props: Props) {
-  const { src, alt, w, className, objFit, objOps, priority = false } = props;
+  const { src, alt, w, className, objFit, objPos, priority = false } = props;
 
   let style = {};
   const responsiveWidth = { width: "100%", height: "auto" };
+
+  const noSemiColons = (sty: string): boolean => {
+    if (sty.includes(";")) {
+      const errorMsg =
+        "Warning: Style property values shouldn't contain a semicolon.";
+      console.error(errorMsg);
+      return false;
+    }
+    return true;
+  };
+
+  if (src == "" || src == null) {
+    const errorMsg = `Image src value is invalid and cannot be empty or null.`;
+    console.error(errorMsg);
+  }
+
+  if (!alt) {
+    const errorMsg = `You should supply a descriptive text for the image, or put as empty("") for decorative image.`;
+    console.error(errorMsg);
+  }
 
   if (!w) {
     style = { ...responsiveWidth };
@@ -30,14 +50,24 @@ export default function Image(props: Props) {
     const objFitValueFound = objFitPossibleValue.find(
       (element) => element === objFit
     );
-
-    if (objFitValueFound) {
-      style = { ...style, objectFit: objFit };
-    } else {
-      const errorMsg =
-        "Value inserted to objFit is invalid, please refer to https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit";
-      console.error(errorMsg);
+    if (noSemiColons(objFit)) {
+      if (objFitValueFound) {
+        style = { ...style, objectFit: objFit };
+      } else {
+        const errorMsg =
+          "Value inserted to objFit is invalid, please refer to https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit.";
+        console.error(errorMsg);
+      }
     }
+  }
+
+  if (objPos) {
+    if (noSemiColons(objPos)) style = { ...style, objectPosition: objPos };
+  }
+
+  if (typeof priority !== "boolean") {
+    const errorMsg = `Value inserted to priority is using ${typeof priority} instead of boolean type, which may lead to unexpected outcome.`;
+    console.error(errorMsg);
   }
 
   return (
